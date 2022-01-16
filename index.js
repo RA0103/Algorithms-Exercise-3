5e86e378ee97b39cda49d79b2b8e92e98455d0af
ilyosbek <ilyosbek.inomjonov.1996@gmail.com>
2022-01-16 10:07:42 +0500

    done

---
+++ index.js
@@ -0,0 +1,16 @@
+// Create a function that takes two arguments. Both arguments are integers, a and b. Return true if one of them is 10 or if their sum is 10.
+
+// Examples
+
+// makesTen(9, 10) ➞ true
+
+// makesTen(9, 9) ➞ false
+
+// makesTen(1, 9) ➞ true
+
+function makesTen(a, b) {
+  if (a == 10 || b == 10) console.log(true);
+  else if (a + b == 10) console.log(true);
+  else console.log(false);
+}
+makesTen(9, 10);
